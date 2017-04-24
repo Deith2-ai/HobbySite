7cf06df28ce683ff2d709d6e06df20950685ce64
Dariusz Łukasik <dariusz.lukasik90@gmail.com>
2017-04-24 20:13:25 +0200

    Admin LTE

--- src/Hobby.Web/App_Start/BundleConfig.cs
+++ src/Hobby.Web/App_Start/BundleConfig.cs
@@ -8,6 +8,8 @@ namespace Hobby.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            RegisterLayout(bundles);
+
             bundles.Add(new ScriptBundle("~/bundles/angularjs").Include(
                          "~/Scripts/angular.js"));
 
@@ -30,9 +32,46 @@ namespace Hobby.Web
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
 
+            bundles.Add(new StyleBundle("~/Scripts/AdminLTE/CSS").Include(
+                    "~/Scripts/AdminLTE/bootstrap/css",
+                    "~/Scripts/AdminLTE/dist/css",
+                    "~/Scripts/AdminLTE/dist/skins/css"));
+
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
-            BundleTable.EnableOptimizations = true;
+            BundleTable.EnableOptimizations = false;
+        }
+
+        public static void RegisterLayout(BundleCollection bundles)
+        {
+            // plugins | font-awesome
+            //bundles.Add(new StyleBundle("~/Scripts/AdminLTE/plugins/font-awesome/css").Include(
+            //    "~/Scripts/AdminLTE/plugins/font-awesome/css/font-awesome.min.css"));
+
+            //// plugins | ionicons
+            //bundles.Add(new StyleBundle("~/Scripts/AdminLTE/plugins/ionicons/css").Include(
+            //    "~/Scripts/AdminLTE/plugins/ionicons/css/ionicons.min.css"));
+
+            // bootstrap
+            bundles.Add(new ScriptBundle("~/Scripts/AdminLTE/bootstrap/js").Include(
+                "~/Scripts/AdminLTE/bootstrap/js/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/Scripts/AdminLTE/bootstrap/css").Include(
+                "~/Scripts/AdminLTE/bootstrap/css/bootstrap.min.css"));
+
+            //// dist
+            bundles.Add(new ScriptBundle("~/Scripts/AdminLTE/dist/js").Include(
+               "~/Scripts/AdminLTE/dist/js/app.js"));
+
+            bundles.Add(new StyleBundle("~/Scripts/AdminLTE/dist/css").Include(
+                "~/Scripts/AdminLTE/dist/css/admin-lte.min.css"));
+
+            bundles.Add(new StyleBundle("~/Scripts/AdminLTE/dist/css/skins").Include(
+                "~/Scripts/AdminLTE/dist/css/skins/_all-skins.min.css"));
+
+            // plugins | slimscroll
+            //bundles.Add(new ScriptBundle("~/Scripts/AdminLTE/plugins/slimscroll/js").Include(
+            //     "~/Scripts/AdminLTE/plugins/slimscroll/js/jquery.slimscroll.min.js"));
         }
     }
 }
